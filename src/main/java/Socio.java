f564f0786878bfae9bf6bd7532e8f0e0e8f66fd7
v <you@example.com>
2014-04-07 18:30:22 +0200

    1.0

---
+++ src/main/java/Socio.java
@@ -0,0 +1,58 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="Socio_table")
+public class Socio {
+	@Id @GeneratedValue
+	int userId;
+	@Column(name="Socio_Nombre")
+	private String nombre;
+	private int numero;
+	private String apellidos;
+	private String comentarios;
+		
+	public Socio(){}
+	
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(String comentarios) {
+		this.comentarios = comentarios;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	
+}
+
